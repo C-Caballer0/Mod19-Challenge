6eed788cb41cf87a6363f1617fa952abe6e98080
Carlo Caballero <carlopcaballero@gmail.com>
2023-03-26 17:56:20 -0400

    Reupload

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,35 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => { 
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.getAll();
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+};
+
+export const getDb = async () => { 
+  console.log('PUT request to update the jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ id: id, value: value });
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+};
+
+initdb();
